40d5b487162d5326f09ceab8a5300a9e8298b680
Shaddy0-0 <barrettsherrilee@gmail.com>
2023-03-23 16:54:52 -0700

    Task11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -13,8 +13,8 @@ int main(void)
 
 	for (i = 3; i < 782849; i = i + 2)
 	{
-		while ((n % i == 0) && (n !=i))
-			n = n /i;
+		while ((n % i == 0) && (n != i))
+			n = n / i;
 	}
 	printf("%lu\n", n);
 	return (0);
